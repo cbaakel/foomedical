b52cb1e96c658b5727e9bbbb832fe2647579f229
Charles Baakel <charles@threepound.com>
2024-05-21 22:19:34 -0700

    reminder to change medplum project id in config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Default values work on localhost:3000 and foomedical.com
 // Replace these values with your own values for production
-export const MEDPLUM_PROJECT_ID = '69213b96-3a97-47c7-8460-d34617df7e1f';
+export const MEDPLUM_PROJECT_ID = 'de35a9b9-dac0-4b46-a250-6285cda970b7';
 export const MEDPLUM_GOOGLE_CLIENT_ID = '679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com';
 export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfFd_8gAAAAAOCVrZQ_aF2CN5b7s91NEYIu5GxL';
